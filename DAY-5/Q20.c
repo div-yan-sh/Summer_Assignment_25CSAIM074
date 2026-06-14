25e680051570757c93984e564f09cf517ff1a117
Divyansh Pratap Singh <divyanshpratapsingh009@gmail.com>
2026-06-14 09:53:31 +0530

    Update Q20.c

--- DAY-5/Q20.c
+++ DAY-5/Q20.c
@@ -1,2 +1,10 @@
 #include<stdio.h>
-int main(){long long n,f=1,i;scanf("%lld",&n);for(i=2;i<=n;i++)while(n%i==0){f=i;n/=i;}printf("%lld",f);}
+int main()
+{long long n,f=1,i;
+ scanf("%lld",&n);
+ for(i=2;i<=n;i++)
+   while(n%i==0){
+     f=i;n/=i;
+   }
+ printf("%lld",f);
+}
