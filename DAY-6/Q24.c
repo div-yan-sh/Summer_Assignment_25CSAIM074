79a8e9cc77206671d11225b2b61afa6bda507b66
Divyansh Pratap Singh <divyanshpratapsingh009@gmail.com>
2026-06-14 09:56:34 +0530

    Update Q24.c

--- DAY-6/Q24.c
+++ DAY-6/Q24.c
@@ -1,2 +1,7 @@
 #include<stdio.h>
-int main(){long long x,n,r=1;scanf("%lld%lld",&x,&n);while(n--)r*=x;printf("%lld",r);}
+int main(){
+  long long x,n,r=1;
+  scanf("%lld%lld",&x,&n);
+  while(n--)r*=x;
+  printf("%lld",r);
+}
