a1d95a42f611286833b03af46e149e7a76466a34
Divyansh Pratap Singh <divyanshpratapsingh009@gmail.com>
2026-06-14 09:50:16 +0530

    Update Q17.c

--- DAY-5/Q17.c
+++ DAY-5/Q17.c
@@ -1,2 +1,8 @@
 #include<stdio.h>
-int main(){int n,i,sum=1;scanf("%d",&n);for(i=2;i<=n/2;i++)if(n%i==0)sum+=i;printf(sum==n&&n>1?"Perfect":"Not Perfect");}
+int main(){
+  int n,i,sum=1;
+           scanf("%d",&n);
+           for(i=2;i<=n/2;i++)
+             if(n%i==0)sum+=i;
+           printf(sum==n&&n>1?"Perfect":"Not Perfect");
+          }
