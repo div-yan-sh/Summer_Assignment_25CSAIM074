6a6dd401a5f9dddbc4d31886fe54f39b7dee0674
Divyansh Pratap Singh <divyanshpratapsingh009@gmail.com>
2026-06-14 09:52:05 +0530

    Update Q18.c

--- DAY-5/Q18.c
+++ DAY-5/Q18.c
@@ -1,2 +1,12 @@
 #include<stdio.h>
-int f(int n){int r=1;while(n){r*=n--; }return r;} int main(){int n,t,s=0;scanf("%d",&n);t=n;while(t){s+=f(t%10);t/=10;}printf(s==n?"Strong":"Not Strong");}
+int f(int n){
+  int r=1;
+  while(n){r*=n--; }
+  return r;} 
+int main(){
+  int n,t,s=0;
+  scanf("%d",&n);
+  t=n;
+  while{s+=f(t%10);t/=10;}
+  printf(s==n?"Strong":"Not Strong");
+}
