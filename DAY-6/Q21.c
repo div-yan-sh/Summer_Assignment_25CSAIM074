34ae0728a1847e01d3c91f7375457d1ee22e11a6
Divyansh Pratap Singh <divyanshpratapsingh009@gmail.com>
2026-06-14 09:54:43 +0530

    Update Q21.c

--- DAY-6/Q21.c
+++ DAY-6/Q21.c
@@ -1,2 +1,9 @@
 #include<stdio.h>
-int main(){int n,a[32],i=0;scanf("%d",&n);do{a[i++]=n%2;n/=2;}while(n);while(i--)printf("%d",a[i]);}
+int main(){
+  int n,a[32],i=0;
+  scanf("%d",&n);
+  do{a[i++]=n%2;n/=2;}
+    while(n);
+  while(i--)
+    printf("%d",a[i]);
+}
