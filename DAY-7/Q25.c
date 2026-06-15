76895045c0de0d8a5fe9cd64a4121fd4b82a768a
Divyansh Pratap Singh <divyanshpratapsingh009@gmail.com>
2026-06-15 20:07:58 +0530

    Update Q25.c

--- DAY-7/Q25.c
+++ DAY-7/Q25.c
@@ -1,2 +1,9 @@
 #include<stdio.h>
-long long f(int n){return n<2?1:n*f(n-1);} int main(){int n;scanf("%d",&n);printf("%lld",f(n));}
+long long f(int n){
+  return n<2?1:n*f(n-1);
+} 
+int main()
+{int n;
+ scanf("%d",&n);
+ printf("%lld",f(n));
+}
