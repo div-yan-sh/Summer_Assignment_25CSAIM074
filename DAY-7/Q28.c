2474785cbbb80b68275fe6c933dba85cfb7679df
Divyansh Pratap Singh <divyanshpratapsingh009@gmail.com>
2026-06-15 20:10:15 +0530

    Update Q28.c

--- DAY-7/Q28.c
+++ DAY-7/Q28.c
@@ -1,2 +1,11 @@
 #include<stdio.h>
-void r(int n){if(!n)return; printf("%d",n%10); r(n/10);} int main(){int n;scanf("%d",&n);r(n);}
+void r(int n){
+  if(!n)return;
+  printf("%d",n%10);
+  r(n/10);
+} 
+int main(){
+  int n;
+  scanf("%d",&n);
+  r(n);
+}
