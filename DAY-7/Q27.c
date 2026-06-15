2f661b45935f388f83a70d780186bc3e39710124
Divyansh Pratap Singh <divyanshpratapsingh009@gmail.com>
2026-06-15 20:09:24 +0530

    Update Q27.c

--- DAY-7/Q27.c
+++ DAY-7/Q27.c
@@ -1,2 +1,9 @@
 #include<stdio.h>
-int s(int n){return n?n%10+s(n/10):0;} int main(){int n;scanf("%d",&n);printf("%d",s(n));}
+int s(int n){
+  return n?n%10+s(n/10):0;
+} 
+int main(){
+  int n;
+  scanf("%d",&n);
+  printf("%d",s(n));
+}
