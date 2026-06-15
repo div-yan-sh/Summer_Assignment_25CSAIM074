f3f13e62a60a8efcaf9af4799f98e84f5e9ef0e5
Divyansh Pratap Singh <divyanshpratapsingh009@gmail.com>
2026-06-15 20:08:50 +0530

    Update Q26.c

--- DAY-7/Q26.c
+++ DAY-7/Q26.c
@@ -1,2 +1,8 @@
 #include<stdio.h>
-int f(int n){return n<2?n:f(n-1)+f(n-2);} int main(){int n;scanf("%d",&n);printf("%d",f(n));}
+int f(int n){
+  return n<2?n:f(n-1)+f(n-2);
+} 
+int main(){
+  int n;scanf("%d",&n);
+  printf("%d",f(n));
+}
