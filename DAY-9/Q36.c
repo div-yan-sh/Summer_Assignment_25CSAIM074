02bbfee28f73266f3e2b92bcf418f010228a7085
Divyansh Pratap Singh <divyanshpratapsingh009@gmail.com>
2026-06-29 20:58:38 +0530

    Update Q36.c

--- DAY-9/Q36.c
+++ DAY-9/Q36.c
@@ -1,2 +1,14 @@
-#include<stdio.h>
-int main(){printf("Pattern program for Q36\n");return 0;}
+#include <stdio.h>
+int main() {
+    int i, j, n = 5;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (i == 0 || i == n-1 || j == 0 || j == n-1)
+                printf("* ");
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
