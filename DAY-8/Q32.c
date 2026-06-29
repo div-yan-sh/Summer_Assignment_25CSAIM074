851ba40d25ec753024191ad0d6dcb6ad5893195e
Divyansh Pratap Singh <divyanshpratapsingh009@gmail.com>
2026-06-29 20:46:31 +0530

    Update Q32.c

--- DAY-8/Q32.c
+++ DAY-8/Q32.c
@@ -1,2 +1,13 @@
-#include<stdio.h>
-int main(){printf("Pattern program for Q32\n");return 0;}
+#include <stdio.h>
+
+int main() {
+    int i, j, rows = 5;
+
+    /* Repeated number: 1 / 22 / 333 ... */
+    for (i = 1; i <= rows; i++) {
+        for (j = 0; j < i; j++)
+            printf("%d", i);
+        printf("\n");
+    }
+    return 0;
+}
