8015c03284a41d809809ec80e6df17338b824078
Divyansh Pratap Singh <divyanshpratapsingh009@gmail.com>
2026-06-29 20:46:00 +0530

    Update Q30.c

--- DAY-8/Q30.c
+++ DAY-8/Q30.c
@@ -1,2 +1,13 @@
-#include<stdio.h>
-int main(){printf("Pattern program for Q30\n");return 0;}
+#include <stdio.h>
+
+int main() {
+    int i, j, rows = 5;
+
+    /* Number triangle: 1 / 12 / 123 ... */
+    for (i = 1; i <= rows; i++) {
+        for (j = 1; j <= i; j++)
+            printf("%d", j);
+        printf("\n");
+    }
+    return 0;
+}
