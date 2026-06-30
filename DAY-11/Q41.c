034bba53ed2b8ff8efe84b1c25bb3241101b154f
Divyansh Pratap Singh <divyanshpratapsingh009@gmail.com>
2026-06-30 09:13:39 +0530

    Update Q41.c

--- DAY-11/Q41.c
+++ DAY-11/Q41.c
@@ -1,2 +1,9 @@
 #include<stdio.h>
-int sum(int a,int b){return a+b;} int main(){int a,b;scanf("%d%d",&a,&b);printf("%d",sum(a,b));}
+int sum(int a,int b){
+  return a+b;
+} 
+int main(){
+  int a,b;
+  scanf("%d%d",&a,&b);
+  printf("%d",sum(a,b));
+}
