fc428e4fbebe463e3f8c3a265d6f7b1bf93c2c0e
Divyansh Pratap Singh <divyanshpratapsingh009@gmail.com>
2026-06-30 09:23:51 +0530

    Format all DAY-15 files (Q57-Q60) with proper indentation and spacing

--- DAY-15/Q60.c
+++ DAY-15/Q60.c
@@ -1,2 +1,17 @@
-#include<stdio.h>
-int main(){int n,a[100],i,j=0,b[100];scanf("%d",&n);for(i=0;i<n;i++)scanf("%d",&a[i]);for(i=0;i<n;i++)if(a[i])b[j++]=a[i];while(j<n)b[j++]=0;for(i=0;i<n;i++)printf("%d ",b[i]);}
+#include <stdio.h>
+
+int main() {
+    int n, a[100], i, j = 0, b[100];
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    for (i = 0; i < n; i++) {
+        if (a[i])
+            b[j++] = a[i];
+    }
+    while (j < n)
+        b[j++] = 0;
+    for (i = 0; i < n; i++)
+        printf("%d ", b[i]);
+    return 0;
+}
