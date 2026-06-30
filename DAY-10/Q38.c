4425b190a88b6720462d908623c14f17008fd6fb
Divyansh Pratap Singh <divyanshpratapsingh009@gmail.com>
2026-06-30 08:50:35 +0530

    Update Q38.c

--- DAY-10/Q38.c
+++ DAY-10/Q38.c
@@ -1,2 +1,12 @@
-#include<stdio.h>
-int main(){printf("Pattern program for Q38\n");return 0;}
+#include <stdio.h>
+int main() {
+    int i, j, n = 5;
+    for (i = n; i >= 1; i--) {
+        for (j = i; j < n; j++)
+            printf(" ");
+        for (j = 1; j <= (2 * i - 1); j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
