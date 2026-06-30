43838674c1400886aebc9a9ecdda40e05b39f2d5
Divyansh Pratap Singh <divyanshpratapsingh009@gmail.com>
2026-06-30 09:28:21 +0530

    Implement DAY-18 (Q69-Q72) solutions with proper indentation and spacing

--- DAY-18/Q70.c
+++ DAY-18/Q70.c
@@ -1,6 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
-    printf("Solution template for Q70\n");
+    int n, a[100], i, j, minidx, temp;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    for (i = 0; i < n - 1; i++) {
+        minidx = i;
+        for (j = i + 1; j < n; j++) {
+            if (a[j] < a[minidx])
+                minidx = j;
+        }
+        temp = a[i];
+        a[i] = a[minidx];
+        a[minidx] = temp;
+    }
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
     return 0;
 }
--- DAY-18/Q71.c
+++ DAY-18/Q71.c
@@ -1,6 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
-    printf("Solution template for Q71\n");
+    int n, a[100], i, x, low, high, mid, found = 0;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    scanf("%d", &x);
+    low = 0;
+    high = n - 1;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (a[mid] == x) {
+            printf("%d", mid);
+            found = 1;
+            break;
+        } else if (a[mid] < x) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    if (!found)
+        printf("-1");
     return 0;
 }
