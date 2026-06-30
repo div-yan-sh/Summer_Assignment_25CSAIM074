5c2ead25d56c0f9d5f9e0eb4df892d67c7f3188e
Divyansh Pratap Singh <divyanshpratapsingh009@gmail.com>
2026-06-30 09:14:19 +0530

    Update Q42.c

--- DAY-11/Q42.c
+++ DAY-11/Q42.c
@@ -1,2 +1,7 @@
 #include<stdio.h>
-int max(int a,int b){return a>b?a:b;} int main(){int a,b;scanf("%d%d",&a,&b);printf("%d",max(a,b));}
+int max(int a,int b){
+  return a>b?a:b;
+} int main(){
+  int a,b;scanf("%d%d",&a,&b);
+  printf("%d",max(a,b));
+}
