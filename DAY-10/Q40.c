9b88c97357a4250f9b0c6c0a0dda7ff096b88e6a
Divyansh Pratap Singh <divyanshpratapsingh009@gmail.com>
2026-06-30 08:50:59 +0530

    Update Q40.c

--- DAY-10/Q40.c
+++ DAY-10/Q40.c
@@ -1,2 +1,14 @@
-#include<stdio.h>
-int main(){printf("Pattern program for Q40\n");return 0;}
+#include <stdio.h>
+int main() {
+    int i, j, n = 5;
+    for (i = 1; i <= n; i++) {
+        for (j = i; j < n; j++)
+            printf(" ");
+        for (j = 0; j < i; j++)
+            printf("%c", 'A' + j);
+        for (j = i - 2; j >= 0; j--)
+            printf("%c", 'A' + j);
+        printf("\n");
+    }
+    return 0;
+}
