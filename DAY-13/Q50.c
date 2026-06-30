5a2364ad4561217d735ad188296e78081e1c12f7
Divyansh Pratap Singh <divyanshpratapsingh009@gmail.com>
2026-06-30 09:21:01 +0530

    Format Q49 and Q50 with proper indentation and spacing

--- DAY-13/Q50.c
+++ DAY-13/Q50.c
@@ -1,2 +1,12 @@
-#include<stdio.h>
-int main(){int n,a[100],i,s=0;scanf("%d",&n);for(i=0;i<n;i++){scanf("%d",&a[i]);s+=a[i];}printf("Sum=%d Avg=%.2f",s,(float)s/n);}
+#include <stdio.h>
+
+int main() {
+    int n, a[100], i, s = 0;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+        s += a[i];
+    }
+    printf("Sum=%d Avg=%.2f", s, (float)s / n);
+    return 0;
+}
