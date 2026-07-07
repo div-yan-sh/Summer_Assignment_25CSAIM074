979e58203109335f02bb0338a85377d02e857430
Divyansh Pratap Singh <divyanshpratapsingh009@gmail.com>
2026-07-07 08:44:53 +0530

    Update Q106.c

--- DAY-27/Q106.c
+++ DAY-27/Q106.c
@@ -1,6 +1,35 @@
-#include<stdio.h>
+#include <stdio.h>
+
+struct Employee {
+    int id;
+    char name[50];
+    char dept[30];
+    char designation[30];
+};
 
 int main() {
-    printf("Solution template for Q106\n");
-    return 0;
+    struct Employee emps[100];
+    int count = 0, choice;
+
+    while(1) {
+        printf("\n--- Employee Management ---");
+        printf("\n1. Add Employee\n2. Display Employees\n3. Exit\nEnter Choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1:
+                printf("Enter ID: "); scanf("%d", &emps[count].id);
+                printf("Enter Name: "); scanf(" %[^\n]s", emps[count].name);
+                printf("Enter Department: "); scanf(" %[^\n]s", emps[count].dept);
+                printf("Enter Designation: "); scanf(" %[^\n]s", emps[count].designation);
+                count++;
+                break;
+            case 2:
+                for(int i=0; i<count; i++) {
+                    printf("ID: %d | Name: %s | Dept: %s | Desg: %s\n", emps[i].id, emps[i].name, emps[i].dept, emps[i].designation);
+                }
+                break;
+            case 3: return 0;
+        }
+    }
 }
