1ba9d4a3ff1bd00b50c272e3b797584ba0adb2a1
Divyansh Pratap Singh <divyanshpratapsingh009@gmail.com>
2026-07-07 08:47:52 +0530

    Update Q115.c

--- DAY-29/Q115.c
+++ DAY-29/Q115.c
@@ -1,6 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 
 int main() {
-    printf("Solution template for Q115\n");
-    return 0;
+    char str[100];
+    int choice;
+
+    printf("Enter string: ");
+    scanf("%[^\n]s", str);
+
+    while(1) {
+        printf("\n1. Length\n2. Reverse\n3. Exit\nChoice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1:
+                printf("Length: %lu\n", strlen(str));
+                break;
+            case 2:
+                printf("Reversed String: ");
+                for(int i = strlen(str) - 1; i >= 0; i--) printf("%c", str[i]);
+                printf("\n");
+                break;
+            case 3: return 0;
+        }
+    }
 }
