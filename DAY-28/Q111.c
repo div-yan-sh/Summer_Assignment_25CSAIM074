7156c281a77071e3aa2ed08034cbf32204fd14a8
Divyansh Pratap Singh <divyanshpratapsingh009@gmail.com>
2026-07-07 08:46:45 +0530

    Update Q111.c

--- DAY-28/Q111.c
+++ DAY-28/Q111.c
@@ -1,6 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
-    printf("Solution template for Q111\n");
-    return 0;
+    int total_seats = 50, booked, choice;
+
+    while(1) {
+        printf("\nAvailable Seats: %d\n1. Book Tickets\n2. Exit\nChoice: ", total_seats);
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1:
+                printf("Enter seats to book: "); scanf("%d", &booked);
+                if(booked <= total_seats) {
+                    total_seats -= booked;
+                    printf("Tickets successfully booked!\n");
+                } else {
+                    printf("Not enough seats available.\n");
+                }
+                break;
+            case 2: return 0;
+        }
+    }
 }
